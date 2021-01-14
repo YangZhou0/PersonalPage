eb9947f52dfe63e944d0390bfb7abccdae31d0b1
Yang Zhou <yzhou3991@gmail.com>
2021-01-14 02:15:42 -0500

    use react-markdown to render markdown with links in it

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
 
 class Resume extends Component {
   render() {
@@ -8,7 +10,7 @@ class Resume extends Component {
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        <p><ReactMarkdown plugins={[gfm]} children={education.description} /></p></div>
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
